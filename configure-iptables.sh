f1009a07df4377f7366092f0dfee5b41d5fe6b52
Aaron Griffin <aaron@aaronscottgriffin.com>
2014-04-06 21:19:35 -0700

    modified:   configure-iptables.sh 	new file:   setup-linux.sh 	new file:   sshd_config

--- configure-iptables.sh
+++ configure-iptables.sh
@@ -4,12 +4,21 @@ iptables -F
 iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -I INPUT 1 -i lo -j ACCEPT
 echo "Protecting against SSH brute force..."
-iptables -A INPUT -i eth0.103 -p tcp -m tcp --dport 22 -m state --state NEW -m recent --set --name SSH --rsource
-iptables -A INPUT -i eth0.103 -p tcp -m tcp --dport 22 -m recent --update --seconds 30 --hitcount 3 --rttl --name SSH --rsource -j REJECT --reject-with tcp-reset
-iptables -A INPUT -i eth0.103 -p tcp -m tcp --dport 22 -j ACCEPT
+iptables -A INPUT -p tcp --dport 22 -m state --state NEW -m recent --update --seconds 60 --hitcount 4 -j DROP
+iptables -A INPUT -p tcp --dport 22 -m state --state NEW -m recent --set
+#iptables -A INPUT -i eth0 -p tcp -m tcp --dport 22 -m state --state NEW -m recent --set --name SSH --rsource
+#iptables -A INPUT -i eth0 -p tcp -m tcp --dport 22 -m recent --update --seconds 30 --hitcount 3 --rttl --name SSH --rsource -j REJECT --reject-with tcp-reset
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+#iptables -A INPUT -i eth0 -p tcp -m tcp --dport 22 -j ACCEPT
 #uncomment for webservers
 #iptables -A INPUT -p tcp -m tcp --dport 80 -m state --state NEW -j ACCEPT
 #iptables -A INPUT -p tcp -m tcp --dport 443 -m state --state NEW -j ACCEPT
+read -p "Is this a web server? (y/n) " yn
+if [ $yn == "y" ] || [ $yn == "Y" ]; then
+	echo "Accepting web traffic..."
+	iptables -A INPUT -p tcp -m tcp --dport 80 -m state --state NEW -j ACCEPT
+	iptables -A INPUT -p tcp -m tcp --dport 443 -m state --state NEW -j ACCEPT
+fi
 echo "Dropping all other traffic..."
 iptables -A INPUT -j LOG
 iptables -A INPUT -j DROP
---
+++ setup-linux.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#Script to configure Linux with secure initial setup
+read -p "Change user password? (y/n) " yn
+if [ $yn == "y" ] || [ $yn == "Y" ]; then
+	passwd
+fi
+read -p "Change root password? (y/n) " yn
+if [ $yn == "y" ] || [ $yn == "Y" ]; then
+	sudo passwd
+fi
+read -p "Configure iptables? (y/n) " yn
+if [ $yn == "y" ] || [ $yn == "Y" ]; then
+	sudo ./configure-iptables.sh
+	if [ -f /etc/debian_version ]; then
+		echo "Saving iptables rules to /etc/iptables/rules.v4"
+		sudo iptables-save > /etc/iptables/rules.v4
+	else
+		echo "Saving iptables rules to /etc/sysconfig/iptables"
+		sudo iptables-save > /etc/sysconfig/iptables
+	fi
+fi
+read -p "Patch SSH configuration? (y/n) " yn
+if [ $yn == "y" ] || [ $yn == "Y" ]; then
+	echo "Backing up current SSH configuration..."
+	sudo mv /etc/ssh/sshd_config /etc/ssh/sshd_config.bk
+	echo "Copying known good configuration..."
+	sudo cp sshd_config /etc/ssh/sshd_config
+fi
+echo "PLEASE REMOVE UNNEEDED USERS"
+cut -d: -f1 /etc/passwd
+read -p "Enter users to remove: " users
+if [ -f /etc/debian_version ]; then
+	for user in $users
+	do
+		echo "Removing $user..."
+		sudo deluser --remove-home $user
+	done
+else
+	for user in $users
+	do
+		echo "Removing $user..."
+		sudo userdel -r $user
+	done
+fi
+echo "PLEASE KILL UNNEEDED/SUSPICOUS PROCESSES"
+sudo lsof -i
+read -p "Enter PID of processes to kill: " processes
+for process in $processes
+do
+	echo "Killing $process..."
+	sudo kill $process
+done
