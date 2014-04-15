5f5bd5a8fc3557f87891e86c5d6b8cc4a8c8462c
Aaron Griffin <aaron@aaronscottgriffin.com>
2014-04-15 12:38:00 -0700

    modified:   search-immutable.sh

--- search-immutable.sh
+++ search-immutable.sh
@@ -1,10 +1,9 @@
 #Searched important directories for immutable files
-echo "Checking for immutable files in /etc/..."
-sudo lsattr -R /etc 2>/dev/null | grep "\-i-"
-echo "Checking for immutable files in /usr/bin/..."
-sudo lsattr -R /usr/bin 2>/dev/null | grep "\-i-"
-echo "Checking for immutable files in /home/..."
-sudo lsattr -R /home 2>/dev/null | grep "\-i-"
-#Takes a really long time to complete for entire filesystem
+#echo "Checking for immutable files in /etc/..."
+#sudo lsattr -R /etc 2>/dev/null | grep "\-i-"
+#echo "Checking for immutable files in /usr/bin/..."
+#sudo lsattr -R /usr/bin 2>/dev/null | grep "\-i-"
+#echo "Checking for immutable files in /home/..."
+#sudo lsattr -R /home 2>/dev/null | grep "\-i-"
 #echo "Searching filesystem for immutable files..."
-#sudo lsattr -R / 2>/dev/null | grep "\-i-"
+sudo lsattr -R / 2>/dev/null | grep "\-i-"
